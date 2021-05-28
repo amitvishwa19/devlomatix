0976493ecac92ad410587461de5b040093e7e75b
jaysvishwagit <jaysvishwa@gmail.com>
2021-05-28 21:02:13 +0530

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('app:cron')->everyTwoMinutes();
+        //$schedule->command('app:cron')->everyTwoMinutes();
+        $schedule->command('app:cron');
     }
 
     /**
