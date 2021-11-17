10853eb2bc9662495017d675b07c0ea75cece96c
jaysvishwagit <jaysvishwa@gmail.com>
2021-11-17 19:38:11 +0530

    local

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
-use App\Services\Setting\Settings;
+//use App\Services\Setting\Settings;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SettingRequest;
 
@@ -17,96 +17,101 @@ class SettingController extends Controller
 
 
     }
-
-    public function index(Request $request,Settings $settings)
-    {
-
-        //dd($settings->get('name'));
-        //dd($settings->all());
-
-        //return Setting::get();
-        return view('admin.pages.setting.setting');
-
-    }
-
-    public function create()
+    public function index(Request $request)
     {
         return view('admin.pages.setting.setting');
-    }
-
-    public function store(Request $request,Settings $setting)
-    {
-        //General setting
-        if($request->type == 'global'){
-            $setting->set('global',$request->name,$request->description);
-
-        }
-
-        // $validate = $request->validate([
-        //     'name' => 'required'
-        // ]);
-
-        // $setting = New Setting;
-        // $setting->name = $request->name;
-        // $setting->save();
-
-        // return redirect()->route('setting.index')
-        // ->with([
-        //     'message'    =>'Setting Saved Successfully',
-        //     'alert-type' => 'success',
-        // ]);
-
-    }
-
-    public function show($id)
-    {
-        $setting = Setting::findOrFail($id);
-
-        return response()->json($setting);
-    }
-
-    public function edit($id)
-    {
-        $setting = Setting::findOrFail($id);
-
-        //return response()->json($setting);
-
-        return view('admin.pages.setting.setting_edit',compact('setting'));
-    }
-
-    public function update(Request $request, $id)
-    {
-
-        $validate = $request->validate([
-            'name' => 'required'
-        ]);
-
-        $setting = Setting::findOrFail($id);
-        $setting->name = $request->name;
-        $setting->save();
-
-        return redirect()->route('setting.index')
-        ->with([
-            'message'    =>'Setting Updated Successfully',
-            'alert-type' => 'success',
-        ]);
 
 
     }
+    // public function index(Request $request,Settings $settings)
+    // {
 
-    public function destroy($id)
-    {
-        $setting = Setting::destroy($id);
+    //     //dd($settings->get('name'));
+    //     //dd($settings->all());
 
-        if($setting){
-            return redirect()->route('setting.index')
-            ->with([
-                'message'    =>'Setting Updated Successfully',
-                'alert-type' => 'success',
-            ]);
-        }else{
+    //     //return Setting::get();
+    //     return view('admin.pages.setting.setting');
 
-        }
+    // }
 
-    }
+    // public function create()
+    // {
+    //     return view('admin.pages.setting.setting');
+    // }
+
+    // public function store(Request $request,Settings $setting)
+    // {
+    //     //General setting
+    //     if($request->type == 'global'){
+    //         $setting->set('global',$request->name,$request->description);
+
+    //     }
+
+    //     // $validate = $request->validate([
+    //     //     'name' => 'required'
+    //     // ]);
+
+    //     // $setting = New Setting;
+    //     // $setting->name = $request->name;
+    //     // $setting->save();
+
+    //     // return redirect()->route('setting.index')
+    //     // ->with([
+    //     //     'message'    =>'Setting Saved Successfully',
+    //     //     'alert-type' => 'success',
+    //     // ]);
+
+    // }
+
+    // public function show($id)
+    // {
+    //     $setting = Setting::findOrFail($id);
+
+    //     return response()->json($setting);
+    // }
+
+    // public function edit($id)
+    // {
+    //     $setting = Setting::findOrFail($id);
+
+    //     //return response()->json($setting);
+
+    //     return view('admin.pages.setting.setting_edit',compact('setting'));
+    // }
+
+    // public function update(Request $request, $id)
+    // {
+
+    //     $validate = $request->validate([
+    //         'name' => 'required'
+    //     ]);
+
+    //     $setting = Setting::findOrFail($id);
+    //     $setting->name = $request->name;
+    //     $setting->save();
+
+    //     return redirect()->route('setting.index')
+    //     ->with([
+    //         'message'    =>'Setting Updated Successfully',
+    //         'alert-type' => 'success',
+    //     ]);
+
+
+    // }
+
+    // public function destroy($id)
+    // {
+    //     $setting = Setting::destroy($id);
+
+    //     if($setting){
+    //         return redirect()->route('setting.index')
+    //         ->with([
+    //             'message'    =>'Setting Updated Successfully',
+    //             'alert-type' => 'success',
+    //         ]);
+    //     }else{
+
+    //     }
+
+    // }
 }
