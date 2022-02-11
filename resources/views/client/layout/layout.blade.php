1380a46bd5107183b7403a62120636fa83630835
jaysvishwagit <jaysvishwa@gmail.com>
2022-02-11 19:11:25 +0530

    local

--- resources/views/client/layout/layout.blade.php
+++ resources/views/client/layout/layout.blade.php
@@ -1,36 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 
-@include('client.partials.header')
+<head>
+   <meta charset="utf-8">
+    <title>Devlomatix</title>
+
+    <!-- Responsive Settings -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    {{-- <link rel="shortcut icon" href="images/favicon.png" id="fav-shortcut" type="image/x-icon"> --}}
+    <link rel="icon" href="{{asset('public/client/images/logo.png')}}" id="fav-icon" type="image/x-icon">
+
+
+	<!--Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
 
 <body>
 
-    <div class="page-wrapper">
-
-
-        <!-- Preloader -->
-        <div class="preloader">
-            {{-- <div class="icon" style="background-image: {{setting('app.logo')}}"></div> --}}
-        </div>
-
-        @include('client.partials.navigation')
-
-        @include('client.partials.search')
-
-        @yield('content')
-
-        @include('client.partials.footer')
-
-        @if(!Request::cookie('cookie_consent'))
-            @include('client.partials.consent')
-        @endif
-
-    </div>
-    <!--End pagewrapper-->
-
-    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
-
-    @include('client.partials.scripts')
+   
 
 </body>
 
