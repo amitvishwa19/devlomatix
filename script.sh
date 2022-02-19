0d96654603f58b782c3e9ca4480596cc20ee4f24
jaysvishwagit <jaysvishwa@gmail.com>
2022-02-19 18:12:10 +0530

    cron

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!bin/sh
-/usr/bin/php /home/u38383999/domains/devlomatix.com/public_html/php artisan schedule:run >> /dev/null 2>&1
+/usr/bin/php /home/u38383999/domains/devlomatix.com/public_html/devlomatix/php artisan schedule:run >> /dev/null 2>&1
