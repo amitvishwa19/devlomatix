65df5b1c08efc62bc7449d8fe3b56c345877ca4b
jaysvishwagit <jaysvishwa@gmail.com>
2022-02-20 22:09:40 +0530

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:cron')->everyMinute();
 
         //$schedule->command('app:deleteactivitylog')->timezone('Asia/Kolkata')->at('13:00');
-        $schedule->command('app:deleteactivitylog')->everyMinute();
+        $schedule->command('app:deleteactivitylog')->everyFiveMinutes();
     }
 
     /**
