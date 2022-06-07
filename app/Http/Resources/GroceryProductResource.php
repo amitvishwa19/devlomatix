df9b1806aeade10d0d071e5ff879b506a582b733
Devlomatix <info@devlomatix.com>
2022-06-07 14:56:43 +0530

    Auto push from command

--- app/Http/Resources/GroceryProductResource.php
+++ app/Http/Resources/GroceryProductResource.php
@@ -17,9 +17,10 @@ class GroceryProductResource extends JsonResource
         return [
             'title' => $this->title,
             'slug' => $this->slug,
-            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'image' => $this->feature_image,
             'price' => $this->description,
+            'sku' => '5',
             'featured' => $this->featured
         ];
     }
