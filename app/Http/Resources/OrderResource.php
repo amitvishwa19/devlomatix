83b4aaab2e1aab27c6080f93d7f6ab418c69c0cf
Devlomatix <info@devlomatix.com>
2022-07-27 13:46:37 +0530

    Auto push from command

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -21,7 +21,7 @@ class OrderResource extends JsonResource
             'address'=>unserialize($this->address),
             'status'=>$this->status,
             'payment_id'=>$this->payment_id,
-            'date' =>  Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->format('D, M d, Y')
+            'date' =>  Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->format('D, M d, Y h:m A' )
         ];
     }
 }
