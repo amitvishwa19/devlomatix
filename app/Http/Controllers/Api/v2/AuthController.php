0a95100dc6e61afa9d89e81ee0af7bf778bd6374
Devlomatix <info@devlomatix.com>
2022-08-02 16:39:01 +0530

    Auto push from command

--- app/Http/Controllers/Api/v2/AuthController.php
+++ app/Http/Controllers/Api/v2/AuthController.php
@@ -96,7 +96,7 @@ class AuthController extends Controller
 
     public function addFirebaseUser(Request $request)
     {
-        return $request->all();
+        //return $request->all();
         $user = User::where('email', $request->email)->first();
         if($user){
             Auth::login($user);
