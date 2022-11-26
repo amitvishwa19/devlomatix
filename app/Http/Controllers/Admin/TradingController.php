84440afc1553f5c6704d478cd4ed823d0ceb5f7c
Devlomatix <info@devlomatix.com>
2022-11-26 16:41:47 +0530

    Auto push from command

--- app/Http/Controllers/Admin/TradingController.php
+++ app/Http/Controllers/Admin/TradingController.php
@@ -119,7 +119,7 @@ class TradingController extends Controller
                 };
 
                 $profit = ($total_sell_amount - $total_buy_amount);
-                $net_traded_value = $profit / round($total_buy_amount,0);
+                $net_traded_value = $profit / 10000;
                 $roi = $total_buy_amount;
                 //$roi = (($total_sell_amount - $total_buy_amount) / $total_buy_amount) * 100;
                 //$roi = round(($total_sell_amount - $total_buy_amount)  / ($total_buy_amount),0);
